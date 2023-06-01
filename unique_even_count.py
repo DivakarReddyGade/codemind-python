37e0c4217a1b7d9f81cff619b702be955a85a3f9
DivakarReddyGade <119839595+DivakarReddyGade@users.noreply.github.com>
2023-06-01 20:24:32 +0530

    n=int(input()) l=list(map(int,input().split())) s=set(l) c=0 c1=0 for i in s:     c=0     for j in l:         if(i==j):             c+=1     if(i%2==0):         c1+=1 print(c1)

---
+++ unique_even_count.py
@@ -0,0 +1,13 @@
+n=int(input())
+l=list(map(int,input().split()))
+s=set(l)
+c=0
+c1=0
+for i in s:
+    c=0
+    for j in l:
+        if(i==j):
+            c+=1
+    if(i%2==0):
+        c1+=1
+print(c1)
