cd85b8b7db829e45a453ac0405b7a3c0783897ec
DivakarReddyGade <119839595+DivakarReddyGade@users.noreply.github.com>
2023-07-15 11:19:47 +0530

    n=int(input()) for i in range(n):     m=int(input())     l=list(map(int,input().split()))     if(sorted(l)==l):         print('0')     else:         print(max(l)-min(l))

--- Optimal_Sorting.py
+++ Optimal_Sorting.py
@@ -2,10 +2,7 @@ n=int(input())
 for i in range(n):
     m=int(input())
     l=list(map(int,input().split()))
-    t=tuple(l)
-    l.sort()
-    if(l==list(t)):
+    if(sorted(l)==l):
         print('0')
     else:
         print(max(l)-min(l))
-    
