2b8df3eaf6540a5dcc94fb59e2a8d5a65af84d78
DivakarReddyGade <119839595+DivakarReddyGade@users.noreply.github.com>
2023-07-20 12:05:08 +0530

    def far(n):     t=n     while(1):         s=t         for i in range(2,s//2):             if s%i==0:                 t=t+1                 break         else:             break     return s def near(n):     t=n     while(1):         s=t         for i in range(2,s//2):             if s%i==0:                 t=t-1                 break         else:             break     return s n=int(input()) for i in range(n):     s=int(input())     a=near(s)     b=far(s)     if(b-s>=s-a):         print(a)     elif(b-s<s-a):         print(b)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,32 +1,32 @@
-def last(n):
-    temp=n
+def far(n):
+    t=n
     while(1):
-        temp1=temp
-        for i in range(2,temp1//2):
-            if temp1%i==0:
-                temp=temp+1
+        s=t
+        for i in range(2,s//2):
+            if s%i==0:
+                t=t+1
                 break
         else:
             break
-    return temp1
-def first(n):
-    temp=n
+    return s 
+def near(n):
+    t=n
     while(1):
-        temp1=temp
-        for i in range(2,temp1//2):
-            if temp1%i==0:
-                temp=temp-1
+        s=t
+        for i in range(2,s//2):
+            if s%i==0:
+                t=t-1
                 break
         else:
             break
-    return temp1  
+    return s  
 n=int(input())
 for i in range(n):
-    temp1=int(input())
-    a=first(temp1)
-    b=last(temp1)
-    if(b-temp1>=temp1-a):
+    s=int(input())
+    a=near(s)
+    b=far(s)
+    if(b-s>=s-a):
         print(a)
-    elif(b-temp1<temp1-a):
+    elif(b-s<s-a):
         print(b)
     
