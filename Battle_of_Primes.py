d7b8cf0ffd34d3a59df2b4f4c890f2f4851433e4
DivakarReddyGade <119839595+DivakarReddyGade@users.noreply.github.com>
2023-07-20 12:41:30 +0530

    a=int(input()) b=int(input()) m=a+b while(1):     for i in range(2,m+1):         if((m+1)%i==0):             m=m+1             break     else:         break print(m+1-(a+b))

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -2,11 +2,10 @@ a=int(input())
 b=int(input())
 m=a+b
 while(1):
-    c=m+1
-    for i in range(2,c):
-        if(c%i==0):
+    for i in range(2,m+1):
+        if((m+1)%i==0):
             m=m+1
             break
     else:
         break
-print(c-(a+b))
+print(m+1-(a+b))
